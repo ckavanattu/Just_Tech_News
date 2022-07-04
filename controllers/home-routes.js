08e7808516f290d19c59c97b6c5054a78b3fe4ed
ckavanattu <chackojk@gmail.com>
2022-07-03 18:35:44 -0700

    added page to view single post and js

--- controllers/home-routes.js
+++ controllers/home-routes.js
@@ -29,7 +29,10 @@ router.get('/', (req, res) => {
     .then(dbPostData => {
         //pass a single post object into the homepage template
         const posts = dbPostData.map(post => post.get({ plain: true }));
-        res.render('homepage', { posts });
+        res.render('homepage', { 
+            posts,
+            loggedIn: req.session.loggedIn
+         });
     })
     .catch(err => {
         console.log(err);
@@ -45,5 +48,49 @@ router.get('/login', (req, res) => {
    
     res.render('login');
 })
+
+router.get('/post/:id', (req, res) => {
+    Post.findOne({
+        where: {
+            id: req.params.id
+        },
+        attributes: [
+            'id',
+            'post_url',
+            'title',
+            'created_at',
+            [sequelize.literal('(SELECT COUNT(*) FROM vote WHERe post.id = vote.post_id)'), 'vote_count']
+        ],
+        include: [
+            {
+                model: Comment,
+                attribute: ['id', 'comment_text', 'post_id', 'user_id', 'created_at'],
+                include:{
+                    model: User,
+                    attributes: ['username']                    
+                }
+            },
+            {
+                model: User,
+                attributes: ['username']
+            }
+        ]
+    })
+    .then(dbPostData=> {
+        if(!dbPostData) {
+            res.status(404).json({ message: 'No post found with this id'});
+            return;
+        }
+        const post = dbPostData.get({plain:true});
+        res.render('single-post', {
+            post,
+            loggedIn: req.session.loggedIn
+        });
+    })
+    .catch(err => {
+        console.log(err);
+        res.status(500).json(err);
+    });
+});
 
 module.exports = router;
